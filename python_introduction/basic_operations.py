dccdc43d80788ffabeb589bf83dea8d900c8000f
greatdignity <emaiwoolabipupo@gmail.com>
2025-07-10 16:37:38 +0100

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+operation = 'Addition'
+number1 = 10
+number2 = 5
+result = number1 + number2
+print(f'Addition of {number1} and {number2} is {result}')
+
+operation = 'Subtraction'
+result = number1 - number2
+print(f'Subtraction of {number1} and {number2} is {result}')
+
+operation = 'Multiplication'
+result = number1 * number2
+print(f'Multiplication of {number1} and {number2} is {result}')
