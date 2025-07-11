dee72281a20829e50d4f4a803fcb0123cf864e75
greatdignity <emaiwoolabipupo@gmail.com>
2025-07-11 08:50:05 +0100

    hours_to_seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,8 @@
-seconds_per_hour = 3600
 hours = 2
+one_munite = 60
+one_hour = 60
+seconds_per_hour = one_munite *one_hour
+
+
 seconds = seconds_per_hour * hours
 print(f'{hours} hour(s) is {seconds} seconds.')
