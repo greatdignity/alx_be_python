9b791b96fa4837062ecceaa26b83be0220557058
greatdignity <emaiwoolabipupo@gmail.com>
2025-07-13 18:22:58 +0100

    hours_to_seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,9 +1,7 @@
 def hours_to_seconds(hour):
     return(hours*60)*60
 hours=2
-r=hours_to_seconds(0)
+r=hours_to_seconds(2)
 seconds=r
-seconds=hours*3600
-
 
 print(f'{hours} hours is {seconds} seconds.')
