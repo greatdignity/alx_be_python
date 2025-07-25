0b9fd16b9a5786f625614157f9827ad79740a294
greatdignity <emaiwoolabipupo@gmail.com>
2025-07-25 14:33:27 +0100

    arithmetic_operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float, num2: float, operation :str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 -num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation =='division':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1/num2
+    else:
+        return "Error: Invalid operation"
+
+print(perform_operation(12.00, 3.00, 'divide'))
