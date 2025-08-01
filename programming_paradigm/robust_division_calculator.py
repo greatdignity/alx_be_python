1aef930a5d6177ed907cda19ae6262e2e17e1220
greatdignity <emaiwoolabipupo@gmail.com>
2025-08-01 10:41:20 +0100

    all

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,5 +10,3 @@ def safe_divide(numerator,denominator):
         return "Error: Please enter numeric values only."
 
 print(f"The result of the division is {safe_divide(10,5)}")
-print(safe_divide(10,0))
-print(safe_divide("ten",5))
